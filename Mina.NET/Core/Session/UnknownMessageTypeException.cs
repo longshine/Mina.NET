4ffabc7bed904671311e64a718b7e401567eec3d
longshine <longxianghe@gmail.com>
2013-11-12 18:40:10 +0800

    UnknownMessageTypeException

---
+++ Mina.NET/Core/Session/UnknownMessageTypeException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Mina.Core.Session
+{
+    /// <summary>
+    /// An exception that is thrown when the type of the message cannot be determined.
+    /// </summary>
+    [Serializable]
+    public class UnknownMessageTypeException : Exception
+    {
+        public UnknownMessageTypeException() { }
+
+        public UnknownMessageTypeException(String message) : base(message) { }
+
+        public UnknownMessageTypeException(String message, Exception inner) : base(message, inner) { }
+
+        protected UnknownMessageTypeException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
