5bc645455083a816cecd6f96599b6316e465c10d
longshine <longxianghe@gmail.com>
2013-11-14 14:42:16 +0800

    StreamIoHandler

---
+++ Mina.NET/Handler/Stream/IoSessionStream.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Mina.Core.Buffer;
+using Mina.Core.Future;
+using Mina.Core.Session;
+
+namespace Mina.Handler.Stream
+{
+    public class IoSessionStream : System.IO.Stream
+    {
+        private volatile Boolean _closed;
+
+        private readonly IoBuffer _buf;
+        private readonly Object _syncRoot;
+        private volatile Boolean _released;
+        private IOException _exception;
+
+        private readonly IoSession _session;
+        private IWriteFuture _lastWriteFuture;
+
+        public IoSessionStream()
+        {
+            _syncRoot = new Byte[0];
+            _buf = ByteBufferAllocator.Instance.Allocate(16);
+            _buf.AutoExpand = true;
+            _buf.Limit = 0;
+        }
+
+        public IoSessionStream(IoSession session)
+        {
+            _session = session;
+        }
+
+        public override Boolean CanRead
+        {
+            get { return _buf != null; }
+        }
+
+        public override Boolean CanSeek
+        {
+            get { return false; }
+        }
+
+        public override Boolean CanWrite
+        {
+            get { return _session != null; }
+        }
+
+        public override Int64 Length
+        {
+            get
+            {
+                if (CanRead)
+                {
+                    if (_released)
+                        return 0;
+                    lock (_syncRoot)
+                    {
+                        return _buf.Remaining;
+                    }
+                }
+                else
+                    throw new NotSupportedException();
+            }
+        }
+
+        public override Int32 ReadByte()
+        {
+            lock (_syncRoot)
+            {
+                if (!WaitForData())
+                    return 0;
+                return _buf.Get() & 0xff;
+            }
+        }
+
+        public override Int32 Read(Byte[] buffer, Int32 offset, Int32 count)
+        {
+            lock (_syncRoot)
+            {
+                if (!WaitForData())
+                    return 0;
+
+                Int32 readBytes = Math.Min(count, _buf.Remaining);
+                _buf.Get(buffer, offset, readBytes);
+                return readBytes;
+            }
+        }
+
+        private Boolean WaitForData()
+        {
+            if (_released)
+                return false;
+
+            lock (_syncRoot)
+            {
+                while (!_released && _buf.Remaining == 0 && _exception == null)
+                {
+                    try
+                    {
+                        Monitor.Wait(_syncRoot);
+                    }
+                    catch (ThreadInterruptedException e)
+                    {
+                        throw new IOException("Interrupted while waiting for more data", e);
+                    }
+                }
+            }
+
+            if (_exception != null)
+            {
+                ReleaseBuffer();
+                throw _exception;
+            }
+
+            if (_closed && _buf.Remaining == 0)
+            {
+                ReleaseBuffer();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ReleaseBuffer()
+        {
+            if (_released)
+                return;
+            _released = true;
+        }
+
+        public override void Write(Byte[] buffer, Int32 offset, Int32 count)
+        {
+            Write(ByteBufferAllocator.Instance.Wrap((Byte[])buffer.Clone(), offset, count));
+        }
+
+        public override void Close()
+        {
+            base.Close();
+
+            if (_closed)
+                return;
+
+            if (_session == null)
+            {
+                lock (_syncRoot)
+                {
+                    _closed = true;
+                    ReleaseBuffer();
+                    Monitor.PulseAll(_syncRoot);
+                }
+            }
+            else
+            {
+                try
+                {
+                    Flush();
+                }
+                finally
+                {
+                    _closed = true;
+                    _session.Close(true).Await();
+                }
+            }
+        }
+
+        public override void Flush()
+        {
+            if (_lastWriteFuture == null)
+                return;
+            _lastWriteFuture.Await();
+            if (!_lastWriteFuture.Written)
+                throw new IOException("The bytes could not be written to the session");
+            _lastWriteFuture = null;
+        }
+
+        public void Write(IoBuffer buf)
+        {
+            if (CanRead)
+            {
+                if (_closed)
+                    return;
+
+                lock (_syncRoot)
+                {
+                    if (_buf.HasRemaining)
+                    {
+                        _buf.Compact().Put(buf).Flip();
+                    }
+                    else
+                    {
+                        _buf.Clear().Put(buf).Flip();
+                        Monitor.PulseAll(_syncRoot);
+                    }
+                }
+            }
+            else if (CanWrite)
+            {
+                if (!_session.Connected)
+                    throw new IOException("The session has been closed.");
+
+                _lastWriteFuture = _session.Write(buf);
+            }
+            else
+                throw new NotSupportedException();
+        }
+
+        public IOException Exception
+        {
+            set
+            {
+                if (_exception == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        _exception = value;
+                        Monitor.PulseAll(_syncRoot);
+                    }
+                }
+            }
+        }
+
+        public override Int64 Position
+        {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+
+        public override Int64 Seek(Int64 offset, System.IO.SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(Int64 value)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Mina.NET/Handler/Stream/StreamIoHandler.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using Common.Logging;
+using Mina.Core.Buffer;
+using Mina.Core.Service;
+using Mina.Core.Session;
+
+namespace Mina.Handler.Stream
+{
+    /// <summary>
+    /// A <see cref="IoHandler"/> that adapts asynchronous MINA events to stream I/O.
+    /// </summary>
+    public abstract class StreamIoHandler : IoHandlerAdapter
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(StreamIoHandler));
+        private static readonly AttributeKey KEY_IN = new AttributeKey(typeof(StreamIoHandler), "in");
+        private static readonly AttributeKey KEY_OUT = new AttributeKey(typeof(StreamIoHandler), "out");
+
+        private UInt32 _readTimeout;
+        private UInt32 _writeTimeout;
+
+        /// <summary>
+        /// Gets or sets read timeout in seconds.
+        /// </summary>
+        public UInt32 ReadTimeout
+        {
+            get { return _readTimeout; }
+            set { _readTimeout = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets write timeout in seconds.
+        /// </summary>
+        public UInt32 WriteTimeout
+        {
+            get { return _writeTimeout; }
+            set { _writeTimeout = value; }
+        }
+
+        public override void SessionOpened(IoSession session)
+        {
+            // Set timeouts
+            session.Config.WriteTimeout = _writeTimeout;
+            session.Config.SetIdleTime(IdleStatus.ReaderIdle, _readTimeout);
+
+            // Create streams
+            IoSessionStream input = new IoSessionStream();
+            IoSessionStream output = new IoSessionStream(session);
+            session.SetAttribute(KEY_IN, input);
+            session.SetAttribute(KEY_OUT, output);
+            ProcessStreamIo(session, input, output);
+        }
+
+        public override void SessionClosed(IoSession session)
+        {
+            IoSessionStream input = session.GetAttribute<IoSessionStream>(KEY_IN);
+            IoSessionStream output = session.GetAttribute<IoSessionStream>(KEY_OUT);
+            try
+            {
+                input.Close();
+            }
+            finally
+            {
+                output.Close();
+            }
+        }
+
+        public override void MessageReceived(IoSession session, Object message)
+        {
+            IoSessionStream input = session.GetAttribute<IoSessionStream>(KEY_IN);
+            input.Write((IoBuffer)message);
+        }
+
+        public override void ExceptionCaught(IoSession session, Exception cause)
+        {
+            IOException ioe = cause as IOException;
+            if (ioe != null)
+            {
+                IoSessionStream input = session.GetAttribute<IoSessionStream>(KEY_IN);
+                if (input != null)
+                {
+                    input.Exception = ioe;
+                    return;
+                }
+            }
+
+            if (log.IsWarnEnabled)
+                log.Warn("Unexpected exception.", cause);
+            session.Close(true);
+        }
+
+        public override void SessionIdle(IoSession session, IdleStatus status)
+        {
+            if (status == IdleStatus.ReaderIdle)
+                throw new IOException("Read timeout");
+        }
+
+        protected abstract void ProcessStreamIo(IoSession session, System.IO.Stream input, System.IO.Stream output);
+    }
+}
