9dd5765eb1e733697c1f99db97f74cf297125d23
longshine <longxianghe@gmail.com>
2014-01-02 18:17:19 +0800

    ReferenceCountingFilter & SessionAttributeInitializingFilter

---
+++ Mina.NET/Filter/Util/ReferenceCountingFilter.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Mina.Core.Filterchain;
+using Mina.Core.Session;
+using Mina.Core.Write;
+
+namespace Mina.Filter.Util
+{
+    /// <summary>
+    /// An <see cref="IoFilter"/> wrapper that keeps track of the number of usages of this filter and will call init/destroy
+    /// when the filter is not in use.
+    /// </summary>
+    public class ReferenceCountingFilter : IoFilterAdapter
+    {
+        private readonly IoFilter _filter;
+        private Int32 _count = 0;
+
+        public ReferenceCountingFilter(IoFilter filter)
+        {
+            _filter = filter;
+        }
+
+        public override void Init()
+        {
+            // no-op, will init on-demand in pre-add if count == 0
+        }
+
+        public override void Destroy()
+        {
+            // no-op, will destroy on-demand in post-remove if count == 0
+        }
+
+        public override void OnPreAdd(IoFilterChain parent, String name, INextFilter nextFilter)
+        {
+            if (_count == 0)
+                _filter.Init();
+            _count++;
+            _filter.OnPreAdd(parent, name, nextFilter);
+        }
+
+        public override void OnPostRemove(IoFilterChain parent, String name, INextFilter nextFilter)
+        {
+            _filter.OnPostRemove(parent, name, nextFilter);
+            _count--;
+            if (_count == 0)
+                _filter.Destroy();
+        }
+
+        public override void OnPostAdd(IoFilterChain parent, String name, INextFilter nextFilter)
+        {
+            _filter.OnPostAdd(parent, name, nextFilter);
+        }
+
+        public override void OnPreRemove(IoFilterChain parent, String name, INextFilter nextFilter)
+        {
+            _filter.OnPreRemove(parent, name, nextFilter);
+        }
+
+        public override void SessionCreated(INextFilter nextFilter, IoSession session)
+        {
+            _filter.SessionCreated(nextFilter, session);
+        }
+
+        public override void SessionOpened(INextFilter nextFilter, IoSession session)
+        {
+            _filter.SessionOpened(nextFilter, session);
+        }
+
+        public override void SessionClosed(INextFilter nextFilter, IoSession session)
+        {
+            _filter.SessionClosed(nextFilter, session);
+        }
+
+        public override void SessionIdle(INextFilter nextFilter, IoSession session, IdleStatus status)
+        {
+            _filter.SessionIdle(nextFilter, session, status);
+        }
+
+        public override void ExceptionCaught(INextFilter nextFilter, IoSession session, Exception cause)
+        {
+            _filter.ExceptionCaught(nextFilter, session, cause);
+        }
+
+        public override void MessageReceived(INextFilter nextFilter, IoSession session, Object message)
+        {
+            _filter.MessageReceived(nextFilter, session, message);
+        }
+
+        public override void MessageSent(INextFilter nextFilter, IoSession session, IWriteRequest writeRequest)
+        {
+            _filter.MessageSent(nextFilter, session, writeRequest);
+        }
+
+        public override void FilterClose(INextFilter nextFilter, IoSession session)
+        {
+            _filter.FilterClose(nextFilter, session);
+        }
+
+        public override void FilterWrite(INextFilter nextFilter, IoSession session, IWriteRequest writeRequest)
+        {
+            _filter.FilterWrite(nextFilter, session, writeRequest);
+        }
+    }
+}
---
+++ Mina.NET/Filter/Util/SessionAttributeInitializingFilter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using Mina.Core.Filterchain;
+using Mina.Core.Session;
+
+namespace Mina.Filter.Util
+{
+    /// <summary>
+    /// An <see cref="IoFilter"/> that sets initial attributes when a new
+    /// <see cref="IoSession"/> is created.  By default, the attribute map is empty when
+    /// an <see cref="IoSession"/> is newly created.  Inserting this filter will make
+    /// the pre-configured attributes available after this filter executes the
+    /// <tt>SessionCreated</tt> event.
+    /// </summary>
+    public class SessionAttributeInitializingFilter : IoFilterAdapter
+    {
+        private readonly Dictionary<String, Object> _attributes = new Dictionary<String, Object>();
+
+        /// <summary>
+        /// Creates a new instance with no default attributes.
+        /// </summary>
+        public SessionAttributeInitializingFilter()
+        { }
+
+        /// <summary>
+        /// Creates a new instance with the specified default attributes.
+        /// </summary>
+        public SessionAttributeInitializingFilter(IDictionary<String, Object> attributes)
+        {
+            Attributes = attributes;
+        }
+
+        /// <summary>
+        /// Gets or sets the attribute map.
+        /// </summary>
+        public IDictionary<String, Object> Attributes
+        {
+            get { return _attributes; }
+            set
+            {
+                _attributes.Clear();
+                if (value != null)
+                {
+                    foreach (KeyValuePair<String, Object> pair in value)
+                    {
+                        _attributes[pair.Key] = pair.Value;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the value of user-defined attribute.
+        /// </summary>
+        /// <param name="key">the key of the attribute</param>
+        /// <returns><tt>null</tt> if there is no attribute with the specified key</returns>
+        public Object GetAttribute(String key)
+        {
+            Object value;
+            return _attributes.TryGetValue(key, out value) ? value : null;
+        }
+
+        /// <summary>
+        /// Sets a user-defined attribute.
+        /// </summary>
+        /// <param name="key">the key of the attribute</param>
+        /// <param name="value">the value of the attribute</param>
+        /// <returns>the old value of the attribute, or <tt>null</tt> if it is new</returns>
+        public Object SetAttribute(String key, Object value)
+        {
+            if (value == null)
+                return RemoveAttribute(key);
+
+            Object old;
+            _attributes.TryGetValue(key, out old);
+            _attributes[key] = value;
+            return old;
+        }
+
+        /// <summary>
+        /// Sets a user defined attribute without a value.
+        /// This is useful when you just want to put a 'mark' attribute.
+        /// </summary>
+        /// <param name="key">the key of the attribute</param>
+        /// <returns>the old value of the attribute, or <tt>null</tt> if it is new</returns>
+        public Object SetAttribute(String key)
+        {
+            return SetAttribute(key, true);
+        }
+
+        /// <summary>
+        /// Removes a user-defined attribute with the specified key.
+        /// </summary>
+        /// <param name="key">the key of the attribute</param>
+        /// <returns>the old value of the attribute, or <tt>null</tt> if not found</returns>
+        public Object RemoveAttribute(String key)
+        {
+            Object old;
+            _attributes.TryGetValue(key, out old);
+            _attributes.Remove(key);
+            return old;
+        }
+
+        /// <summary>
+        /// Returns <tt>true</tt> if this session contains the attribute with
+        /// the specified <tt>key</tt>.
+        /// </summary>
+        public Boolean ContainsAttribute(String key)
+        {
+            return _attributes.ContainsKey(key);
+        }
+
+        public override void SessionCreated(INextFilter nextFilter, IoSession session)
+        {
+            foreach (KeyValuePair<String, Object> pair in _attributes)
+            {
+                session.SetAttribute(pair.Key, pair.Value);
+            }
+
+            base.SessionCreated(nextFilter, session);
+        }
+    }
+}
