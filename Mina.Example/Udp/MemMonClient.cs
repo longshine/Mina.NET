dbd1c315a3b1346ae6d230b27b3d271509751f13
longshine <longxianghe@gmail.com>
2014-04-03 19:17:08 +0800

    udp example

---
+++ Mina.Example/Udp/MemMonClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using Mina.Core.Buffer;
+using Mina.Core.Future;
+using Mina.Core.Service;
+using Mina.Core.Session;
+using Mina.Transport.Socket;
+
+namespace Mina.Example.Udp
+{
+    /// <summary>
+    /// Sends its memory usage to the MemoryMonitor server.
+    /// </summary>
+    class MemMonClient
+    {
+        static void Main(string[] args)
+        {
+            IoConnector connector = new AsyncDatagramConnector();
+
+            connector.ExceptionCaught += (s, e) =>
+            {
+                Console.WriteLine(e.Exception);
+            };
+            connector.MessageReceived += (s, e) =>
+            {
+                Console.WriteLine("Session recv...");
+            };
+            connector.MessageSent += (s, e) =>
+            {
+                Console.WriteLine("Session sent...");
+            };
+            connector.SessionCreated += (s, e) =>
+            {
+                Console.WriteLine("Session created...");
+            };
+            connector.SessionOpened += (s, e) =>
+            {
+                Console.WriteLine("Session opened...");
+            };
+            connector.SessionClosed += (s, e) =>
+            {
+                Console.WriteLine("Session closed...");
+            };
+            connector.SessionIdle += (s, e) =>
+            {
+                Console.WriteLine("Session idle...");
+            };
+
+            IConnectFuture connFuture = connector.Connect(new IPEndPoint(IPAddress.Loopback, MemoryMonitor.port));
+            connFuture.Await();
+
+            connFuture.Complete += (s, e) =>
+            {
+                IConnectFuture f = (IConnectFuture)e.Future;
+                if (f.Connected)
+                {
+                    Console.WriteLine("...connected");
+                    IoSession session = f.Session;
+
+                    for (int i = 0; i < 30; i++)
+                    {
+                        Int64 memory = GC.GetTotalMemory(false);
+                        IoBuffer buffer = IoBuffer.Allocate(8);
+                        buffer.PutInt64(memory);
+                        buffer.Flip();
+                        session.Write(buffer);
+
+                        try
+                        {
+                            Thread.Sleep(1000);
+                        }
+                        catch (ThreadInterruptedException)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not connected...exiting");
+                }
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mina.Example/Udp/MemoryMonitor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using Mina.Core.Buffer;
+using Mina.Filter.Logging;
+using Mina.Transport.Socket;
+
+namespace Mina.Example.Udp
+{
+    /// <summary>
+    /// The class that will accept and process clients in order to properly
+    /// track the memory usage.
+    /// </summary>
+    class MemoryMonitor
+    {
+        public const int port = 18567;
+
+        static void Main(string[] args)
+        {
+            AsyncDatagramAcceptor acceptor = new AsyncDatagramAcceptor();
+
+            acceptor.FilterChain.AddLast("logger", new LoggingFilter());
+
+            acceptor.ExceptionCaught += (s, e) =>
+            {
+                Console.WriteLine(e.Exception);
+                e.Session.Close(true);
+            };
+            acceptor.MessageReceived += (s, e) =>
+            {
+                IoBuffer buf = e.Message as IoBuffer;
+                if (buf != null)
+                {
+                    Console.WriteLine("New value for {0}: {1}", e.Session.RemoteEndPoint, buf.GetInt64());
+                }
+            };
+            acceptor.SessionCreated += (s, e) =>
+            {
+                Console.WriteLine("Session created...");
+            };
+            acceptor.SessionOpened += (s, e) =>
+            {
+                Console.WriteLine("Session opened...");
+            };
+            acceptor.SessionClosed += (s, e) =>
+            {
+                Console.WriteLine("Session closed...");
+            };
+            acceptor.SessionIdle += (s, e) =>
+            {
+                Console.WriteLine("Session idle...");
+            };
+
+            acceptor.Bind(new IPEndPoint(IPAddress.Any, port));
+            Console.WriteLine("UDPServer listening on port " + port);
+            Console.ReadLine();
+        }
+    }
+}
