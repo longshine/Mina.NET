b02d967c44846d59d0e95ac9f386e87903cff930
longshine <longxianghe@gmail.com>
2014-05-30 20:23:49 +0800

    IoEventType should have binary-masked values

--- Mina.NET/Core/Session/IoEventType.cs
+++ Mina.NET/Core/Session/IoEventType.cs
@@ -9,14 +9,15 @@ namespace Mina.Core.Session
     [Flags]
     public enum IoEventType
     {
-        SessionCreated,
-        SessionOpened,
-        SessionClosed,
-        MessageReceived,
-        MessageSent,
-        SessionIdle,
-        ExceptionCaught,
-        Write,
-        Close
+        None = 0,
+        SessionCreated = 0x1,
+        SessionOpened = 0x2,
+        SessionClosed = 0x4,
+        MessageReceived = 0x8,
+        MessageSent = 0x10,
+        SessionIdle = 0x20,
+        ExceptionCaught = 0x40,
+        Write = 0x80,
+        Close = 0x100
     }
 }
